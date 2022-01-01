ededb0652b2a95ffb0e625c7435b1698775342e6
Luv Suneja <luvsuneja@gmail.com>
2022-01-01 17:39:18 +0400

    changes

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -59,7 +59,9 @@ contract StarNotary is ERC721 {
     // Implement Task 1 lookUptokenIdToStarInfo
     function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
-        return tokenIdToStarInfo[_tokenId];
+        Star memory star = tokenIdToStarInfo[_tokenId];
+        string memory starName = star.name;
+        return starName;
 
     }
 
@@ -69,10 +71,11 @@ contract StarNotary is ERC721 {
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
         //4. Use _transferFrom function to exchange the tokens.
-        require((ownerOf(_tokenId1)==msg.sender)||(ownerOf(_tokenId2)==msg.sender), "Owner of _tokenId1 or _tokenId2 needs to be the sender.");
+        address owner1 = ownerOf(_tokenId1);
+        address owner2 = ownerOf(_tokenId2);
+        require((owner1==msg.sender)||(owner2==msg.sender), "not the owner.");
         
-        let owner1 = ownerOf(_tokenId1);
-        let owner2 = ownerOf(_tokenId2);
+        
         _transferFrom(owner1, owner2, _tokenId1);
         _transferFrom(owner2, owner1, _tokenId2);
 
@@ -82,7 +85,8 @@ contract StarNotary is ERC721 {
     function transferStar(address _to1, uint256 _tokenId) public {
         //1. Check if the sender is the ownerOf(_tokenId)
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
-        require(ownerOf(_tokenId)==msg.sender, "The sender shold be the owner of the star.");
+        address owner  = ownerOf(_tokenId); 
+        require(owner==msg.sender, "The sender shold be the owner of the star.");
         _transferFrom(msg.sender, _to1, _tokenId);
     }
 
--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "54279ae6401e4ee0ac1ed6c8a5c11bdc";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -72,6 +72,15 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+      network_id: 4,       // Ropsten's id
+      gas: 4500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {
